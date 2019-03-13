d642d0c1bbdb76dbc62d48c7e6ac749d66f1ded2
KyeKiose <42334353+KyeKiose@users.noreply.github.com>
2019-03-13 18:42:52 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.start_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LimeGreen;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label1.Font = new System.Drawing.Font("Kristen ITC", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(271, 162);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(467, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome on  Bon Appetit";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // start_button
+            // 
+            this.start_button.BackColor = System.Drawing.Color.LimeGreen;
+            this.start_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.start_button.FlatAppearance.BorderColor = System.Drawing.Color.LimeGreen;
+            this.start_button.Font = new System.Drawing.Font("Kristen ITC", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.Location = new System.Drawing.Point(337, 234);
+            this.start_button.Margin = new System.Windows.Forms.Padding(0);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(157, 53);
+            this.start_button.TabIndex = 1;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = false;
+            this.start_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources._18_avocado_dosie_Posta_Magazine;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(858, 565);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Bon Appetit";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button start_button;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    class Product
+    {
+        private string name;
+        private double protein;
+        private double carbon;
+        private double fat;
+        private double ccal;
+        public Product(string name, double protein, double carbon, double fat, double ccal)
+        {
+            this.name = name;
+            this.protein = protein;
+            this.carbon = carbon;
+            this.fat = fat;
+            this.ccal = ccal;
+        }
+    }
+    class Meal : Product
+    {
+        private Product[] meals;
+        public Meal(Product[] meals, string name, double protein, double carbon, double fat, double ccal) : base(name, protein, carbon, fat, ccal)
+        {
+            this.meals = meals;
+        }
+
+    }
+
+    class User
+    {
+         
+    }
+}
